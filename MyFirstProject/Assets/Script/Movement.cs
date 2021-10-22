88304e5b36d4aa6f2b47a60b69c581960cb4a0f3
IrrealV <vmherasdurillo@hotmail.com>
2021-10-22 10:23:30 +0200

    position

---
+++ MyFirstProject/Assets/Script/Movement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform.position = new Vector3(1,2,1
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- MyFirstProject/Assets/Script/Test.cs
+++ MyFirstProject/Assets/Script/Test.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Test : MonoBehaviour
 {
+    public TextMeshProUGUI UItext;
+    public int number = 0;
+    public float hp;
+    public bool b;
+    public Vector3 position;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello Wordl!");
+  
+        UItext.text = position.ToString();
+        Debug.Log(number * hp); 
+        Debug.Log(position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Hola"); 
     }
 }
