14c2a2358272e74372c152a20d4ee8ef29f69cff
IrrealV <vmherasdurillo@hotmail.com>
2022-02-15 10:25:03 +0100

    Mecanismo de disparo
    Se ha añadido movimiento de la bala y las colisiones con un asteroide

--- LucahdorEstrella/Assets/Scripts/SimpleShoot.cs
+++ LucahdorEstrella/Assets/Scripts/SimpleShoot.cs
@@ -7,6 +7,7 @@ public class SimpleShoot : MonoBehaviour
     public GameObject bullet;
     public float Cooldown;
     public Transform SpawnPoint;
+    public float TD;
 
     private float cdCounter = 0;
     // Start is called before the first frame update
@@ -20,11 +21,22 @@ public class SimpleShoot : MonoBehaviour
     {
         cdCounter += Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.Space) && cdCounter > Cooldown)
+        if (Input.GetKeyDown(KeyCode.Space) && cdCounter > Cooldown)
         {
             //instanciamos (creamos) la bala
-            Instantiate(bullet, SpawnPoint.position, Quaternion.identity);
+            GameObject b1 =Instantiate(bullet, SpawnPoint.position, Quaternion.identity);
+            //destruimos la bala despues de un tiempo
+            Destroy(b1, TD);
+            //reseteamos el cd counter a 0
+            cdCounter = 0;
+        };
 
+        if (Input.GetKey(KeyCode.M) && cdCounter > Cooldown)
+        {
+            //instanciamos (creamos) la bala
+            GameObject b1 = Instantiate(bullet, SpawnPoint.position, Quaternion.identity);
+            //destruimos la bala despues de un tiempo
+            Destroy(b1, TD);
             //reseteamos el cd counter a 0
             cdCounter = 0;
         };
