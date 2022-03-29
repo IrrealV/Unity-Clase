72634f500337dfc8ea28df895f17ff051becda78
IrrealV <vmherasdurillo@hotmail.com>
2022-03-29 10:24:04 +0200

    muerte final

--- LucahdorEstrella/Assets/Scripts/UI_Text.cs
+++ LucahdorEstrella/Assets/Scripts/UI_Text.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UI_Text : MonoBehaviour
 {
+    public TextMeshProUGUI content;
+    private float count;
     // Start is called before the first frame update
     void Start()
     {
-        
+        content.text = "Texto de ejemplo";
     }
 
     // Update is called once per frame
